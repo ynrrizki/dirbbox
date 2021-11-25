868ff575be5da1e82fc04452cdfefd21f52f9133
ynrrizki <yanuarrizki165@gmail.com>
2021-11-25 16:05:00 +0700

    menambahkan page login dan image

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: Get.width,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: Routes.LOGIN,
       getPages: AppPages.routes,
     ),
   );
